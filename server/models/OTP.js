337430cc09e1ce2e2884d4095d7e78e4ce46d179
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-10-07 13:52:11 +0530

    Server - sendVerificationMail via nodemailer

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const OTPSchema = new mongoose.Schema({
-    
+
     email: {
         type: String,
         required: true,
@@ -17,5 +17,24 @@ const OTPSchema = new mongoose.Schema({
     },
 
 });
+
+//A Function to send emails
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = await mailSender(email, "Verification mail from StudyNotion", otp);
+        console.log("Email sent successfully", mailResponse);
+    }
+    catch(error) {
+        console.log("Error occurred whiole sending mail: "error);
+        throw error;
+    }
+}
+
+//Pre save middleware, document db me save hone se just pehele ye code run hona chahiye
+OTPSchema.pre("save", async function(next)) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+}
+
 
 module.exports = mongoose.model("OTP", OTPSchema);
